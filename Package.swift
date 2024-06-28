4e525983e5c498fe83b4202c078b233fe9078b4e
David Jangdal <djangdal@gmail.com>
2024-06-28 23:16:16 +0200

    Update Package.swift
    updated package.swift to be a library

--- Package.swift
+++ Package.swift
@@ -4,19 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "RPi",
+    name: "MFRC522",
+    products: [
+        .library(
+            name: "MFRC522",
+            targets: ["MFRC522"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.1.5"),
-        // .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "RPi",
+            name: "MFRC522",
             dependencies: ["SwiftyGPIO"]),
         .testTarget(
             name: "RPiTests",
-            dependencies: ["RPi"]),
+            dependencies: ["MFRC522"]),
     ]
 )
